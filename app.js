2a15119b6ab6366361638223174ef42dac5916aa
Naufal Zaky <naufalzaky754@gmail.com>
2022-08-21 16:14:12 +0700

    push app.js,contact.html,index.html,package.json,pacakge-lock.json
    push app.js,contact.html,index.html,package.json,pacakge-lock.json

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//root
+//route
+
+
+
+app.get('/',(req,res) => {
+    res.sendFile('./index.html', {root: __dirname});
+});
+app.get('/about',(req,res) => {
+    res.sendFile('./about.html', {root: __dirname});
+});
+app.get('/contact',(req,res) => {
+    res.sendFile('./contact.html', {root: __dirname});
+});
+
+app.get('/product/:id', (req,res)=>{
+    res.send(`Product ID : ${req.params.id} <br> Category ID: ${req.params.idCat}`);
+    
+})
+
+app.use('/',(req,res)=>{
+    res.status(404);
+    res.send('<h1>404</h1>');
+})
+app.listen(port,() =>{
+    console.log(`Example app listening at htp://localhost:${port}`);
+});
